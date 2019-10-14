db5cc2285c293c7b9a3216ccb57e54b205bbfe6e
Udit Desai <desaiuditd@gmail.com>
2019-10-14 22:22:01 +1100

    Fix install script.

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-shopt -s dotglob
 wget https://github.com/desaiuditd/wp-skeleton/archive/master.zip
 unzip master.zip
-mv wp-skeleton-master/* .
-mv wp-skeleton-master/.* .
+shopt -s dotglob
+mv wp-skeleton-master/* ./
 rm -rf wp-skeleton-master
 rm -rf master.zip
