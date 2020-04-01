edac92dd3735ad977094aceca342b6e6a68a0297
Udit Desai <desaiuditd@gmail.com>
2020-04-02 00:48:12 +1100

    Upgrade wordpress and add phpcs in lint-staged.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	'*.css': [ 'stylelint' ],
 	'*.js': [ 'eslint webpack.config.js' ],
+	'*.php': [ './vendor/bin/phpcs -p -s -v --standard=phpcs.xml --extensions=php' ],
 	'*.scss': [ 'stylelint --syntax scss' ],
 };
